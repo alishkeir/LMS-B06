bf3e0526961cc408db6abb3e4c65bcc08134249b
Ali Shokair <60306272+alishkeir@users.noreply.github.com>
2023-11-03 20:33:56 +0200

    Changed IDs names

--- _resources/Create Tables.sql
+++ _resources/Create Tables.sql
@@ -1,28 +1,28 @@
 -- Create the Admins table for authentication
 CREATE TABLE admins (
-  admin_id INT PRIMARY KEY AUTO_INCREMENT,
+  ID INT PRIMARY KEY AUTO_INCREMENT,
   username VARCHAR(255),
   password VARCHAR(255)
 );
 
 -- Create the Classes table
 CREATE TABLE classes (
-  class_id INT PRIMARY KEY AUTO_INCREMENT,
+  ID INT PRIMARY KEY AUTO_INCREMENT,
   class_name VARCHAR(255)
 );
 
 -- Create the Sections table
 CREATE TABLE sections (
-  section_id INT PRIMARY KEY AUTO_INCREMENT,
+  ID INT PRIMARY KEY AUTO_INCREMENT,
   section_name VARCHAR(255),
   max_students INT,
   class_id INT,
-  FOREIGN KEY (class_id) REFERENCES classes(class_id)
+  FOREIGN KEY (class_id) REFERENCES classes(ID)
 );
 
 -- Create the Students table
 CREATE TABLE students (
-  student_id INT PRIMARY KEY AUTO_INCREMENT,
+  ID INT PRIMARY KEY AUTO_INCREMENT,
   first_name VARCHAR(255),
   last_name VARCHAR(255),
   email VARCHAR(255),
@@ -30,18 +30,17 @@ CREATE TABLE students (
   picture VARCHAR(255),
   class_id INT,
   section_id INT,
-  FOREIGN KEY (class_id) REFERENCES classes(class_id),
-  FOREIGN KEY (section_id) REFERENCES sections(section_id)
+  FOREIGN KEY (class_id) REFERENCES classes(ID),
+  FOREIGN KEY (section_id) REFERENCES sections(ID)
 );
 
 -- Create the Attendance table
 CREATE TABLE attendance (
-  attendance_id INT PRIMARY KEY AUTO_INCREMENT,
+  ID INT PRIMARY KEY AUTO_INCREMENT,
   student_id INT,
   section_id INT,
   date DATE,
   status VARCHAR(10),
-  FOREIGN KEY (student_id) REFERENCES students(student_id),
-  FOREIGN KEY (section_id) REFERENCES sections(section_id)
+  FOREIGN KEY (student_id) REFERENCES students(ID),
+  FOREIGN KEY (section_id) REFERENCES sections(ID)
 );
-
