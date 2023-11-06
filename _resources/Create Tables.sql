46fa95e90186348fcd6468e46abe47468b2f4019
Ali Shkeir <alishkeir_@hotmail.com>
2023-11-06 03:22:36 +0200

    Added tables constraints and rules

--- _resources/Create Tables.sql
+++ _resources/Create Tables.sql
@@ -17,7 +17,7 @@ CREATE TABLE sections (
   section_name VARCHAR(255),
   max_students INT,
   class_id INT,
-  FOREIGN KEY (class_id) REFERENCES classes(ID)
+  FOREIGN KEY (class_id) REFERENCES classes(ID) ON DELETE RESTRICT
 );
 
 -- Create the Students table
@@ -30,8 +30,8 @@ CREATE TABLE students (
   picture VARCHAR(255),
   class_id INT,
   section_id INT,
-  FOREIGN KEY (class_id) REFERENCES classes(ID),
-  FOREIGN KEY (section_id) REFERENCES sections(ID)
+  FOREIGN KEY (class_id) REFERENCES classes(ID), ON DELETE RESTRICT
+  FOREIGN KEY (section_id) REFERENCES sections(ID) ON DELETE RESTRICT
 );
 
 -- Create the Attendance table
@@ -44,3 +44,30 @@ CREATE TABLE attendance (
   FOREIGN KEY (student_id) REFERENCES students(ID),
   FOREIGN KEY (section_id) REFERENCES sections(ID)
 );
+
+
+
+-- make admin username unique
+ALTER TABLE admins
+ADD CONSTRAINT unique_admin_username UNIQUE (username);
+
+
+-- make class name unique
+ALTER TABLE classes
+ADD CONSTRAINT unique_class_name UNIQUE (class_name);
+
+
+-- make section name unique within a class
+ALTER TABLE sections
+ADD CONSTRAINT unique_section_name_in_class UNIQUE (section_name, class_id);
+
+
+-- make student email and phone_number unique
+ALTER TABLE students
+ADD CONSTRAINT unique_student_email UNIQUE (email),
+ADD CONSTRAINT unique_student_phone UNIQUE (phone_number);
+
+
+-- make sure one student can have only one attendance daily
+ALTER TABLE attendance
+ADD CONSTRAINT unique_student_date UNIQUE (student_id, date);
